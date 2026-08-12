f0f710436b498aa1c4900d3219b89af843f5f366
TAMILNILAVAN K <tamilnilavan232005@gmail.com>
2026-08-12 11:44:20 +0530

    Add: validate-parentheses - submission-0

---
+++ Data Structures & Algorithms/validate-parentheses/submission-0.py
@@ -0,0 +1,15 @@
+from collections import deque
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        closed={')':'(','}':'{',']':'['}
+
+        for i in s:
+            if i in closed:
+                if stack and  stack[-1] == closed[i]:
+                    stack.pop()
+                else :return False
+            else:
+                stack.append(i)
+        
+        return len(stack) == 0
